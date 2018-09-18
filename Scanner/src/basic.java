8740d015f90bc1407a334ef6129acd6cf13627d1
Jason Hernandez <jasonhr781@gmail.com>
2018-09-18 14:48:22 -0400

    classwork scanner

---
+++ Scanner/src/basic.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class basic{
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+		String text = in.nextLine();
+		System.out.println(text);
+		in.close();
+	}
+}
